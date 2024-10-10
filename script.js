16f44cdd03d7ab4b3f07ec642f85039cb9e95535
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-11 00:31:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("inputBox");
+
+let buttons = document.querySelectorAll("button");
+let buttonsArray = Array.from(buttons);
+
+let string = '';
+buttonsArray.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        } else if (e.target.innerHTML == 'AC') {
+            string = '';
+            display.value = string;
+        } else if (e.target.innerHTML == '=') {
+            try {
+                string = eval(string);
+                display.value = string;
+            } catch {
+                display.value = "Error";
+            }
+        } else {
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+    });
+});
+
+
+
